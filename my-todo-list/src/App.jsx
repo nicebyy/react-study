c343397a23cc19e5d059042ca575a6b1c0adb068
katastrophe <45655434+nicebyy@users.noreply.github.com>
2024-06-11 17:57:53 +0900

    feat. Todo-list header,editor

---
+++ my-todo-list/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/Header";
+import Editor from "./components/Editor"
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <Header/>
+      <Editor/>
+    </div>
+  );
+}
+
+export default App;
